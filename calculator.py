de36b82f1beb081b92050cc7e05c20c10c35744b
akauf13 <adammkaufman10@gmail.com>
2023-06-02 12:58:35 -0400

    styling code

--- calculator.py
+++ calculator.py
@@ -2,7 +2,6 @@ first = float(input("First number: "))
 second = float(input("Second number: "))
 total = 0
 
-
 def run():
   operation = input("Type a to add, s to subtract, m to multiply, d to divide, or e for exponent! ")
 
