448f0a60827fcc8bb203c19a91c1d3c647da0a03
akauf13 <adammkaufman10@gmail.com>
2023-06-02 13:18:38 -0400

    modified prompt

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 def run():
-  operation = input("Type a to add, s to subtract, m to multiply, d to divide, e for exponent, or q to quit! ")
+  operation = input("Choose your operation. Type a to add, s to subtract, m to multiply, d to divide, e for exponent, or q to quit! ")
   first = float(input("First number: "))
   second = float(input("Second number: "))
 
