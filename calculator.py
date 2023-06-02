2bc5afb794e11df5e3528f13afb7fc5d850fbbf1
akauf13 <adammkaufman10@gmail.com>
2023-06-02 13:06:18 -0400

    moved variables

--- calculator.py
+++ calculator.py
@@ -1,28 +1,35 @@
-first = float(input("First number: "))
-second = float(input("Second number: "))
+# first = float(input("First number: "))
+# second = float(input("Second number: "))
 
 def run():
+  first = float(input("First number: "))
+  second = float(input("Second number: "))
   operation = input("Type a to add, s to subtract, m to multiply, d to divide, e for exponent, or q to quit! ")
 
   if operation == 'a' or operation == 'A':
       total = first + second
       print(total)
+      run()
 
   elif operation == 's' or operation == 'S':
       total = first - second
       print(total)
+      run()
 
   elif operation == 'm'or operation == 'M':
       total = first * second
       print(total)
+      run()
 
   elif operation == 'd' or operation == 'D':
       total = first / second
       print(total)
+      run()
 
   elif operation == 'e' or operation == 'E':
       total = first ** second
       print(total)
+      run()
 
   elif operation == 'q' or operation == 'Q':
       return
