b141d63e2d5c992af6757a5ca85de1fed26f9e61
akauf13 <adammkaufman10@gmail.com>
2023-06-02 13:10:14 -0400

    cleaned up code

--- calculator.py
+++ calculator.py
@@ -1,6 +1,3 @@
-# first = float(input("First number: "))
-# second = float(input("Second number: "))
-
 def run():
   operation = input("Type a to add, s to subtract, m to multiply, d to divide, e for exponent, or q to quit! ")
   first = float(input("First number: "))
