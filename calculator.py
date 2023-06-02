fcaccce0945dfd5b224674cbb9c80b4424872312
akauf13 <adammkaufman10@gmail.com>
2023-06-02 12:59:22 -0400

    removed total initialization

--- calculator.py
+++ calculator.py
@@ -1,6 +1,5 @@
 first = float(input("First number: "))
 second = float(input("Second number: "))
-total = 0
 
 def run():
   operation = input("Type a to add, s to subtract, m to multiply, d to divide, or e for exponent! ")
