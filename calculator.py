06786ac0efc24f3991ed10c34d95930805a3f437
akauf13 <adammkaufman10@gmail.com>
2023-06-02 12:56:46 -0400

    if adding

--- calculator.py
+++ calculator.py
@@ -8,5 +8,9 @@ total = 0
 def run():
   operation = input("Type a to add, s to subtract, m to multiply, d to divide, or e for exponent! ")
 
-  
+  if operation == 'a' or operation == 'A':
+      total = first + second
+      print(total)
+
+
 run()
