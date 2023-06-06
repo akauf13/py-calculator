115f55d46b383c4343612d0b0e1e3dea3c226d2a
akauf13 <adammkaufman10@gmail.com>
2023-06-06 11:33:34 -0400

    fixed order of prompts

--- calculator.py
+++ calculator.py
@@ -1,29 +1,38 @@
 def run():
   operation = input("Choose your operation. Type a to add, s to subtract, m to multiply, d to divide, e for exponent, or q to quit! ")
-  first = float(input("First number: "))
-  second = float(input("Second number: "))
+
 
   if operation == 'a' or operation == 'A':
+      first = float(input("First number: "))
+      second = float(input("Second number: "))
       total = first + second
       print(total)
       run()
 
   elif operation == 's' or operation == 'S':
+      first = float(input("First number: "))
+      second = float(input("Second number: "))
       total = first - second
       print(total)
       run()
 
   elif operation == 'm'or operation == 'M':
+      first = float(input("First number: "))
+      second = float(input("Second number: "))
       total = first * second
       print(total)
       run()
 
   elif operation == 'd' or operation == 'D':
+      first = float(input("First number: "))
+      second = float(input("Second number: "))
       total = first / second
       print(total)
       run()
 
   elif operation == 'e' or operation == 'E':
+      first = float(input("First number: "))
+      second = float(input("Second number: "))
       total = first ** second
       print(total)
       run()
